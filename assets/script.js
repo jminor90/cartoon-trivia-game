b503820e92bc92b910687e9ae984b3a53cf9106b
Joshua <120212106+jminor90@users.noreply.github.com>
2022-12-26 16:46:42 -0700

    Updated CSS styles to divide all sections for further styling. Updated JS can now track Highscores, and will feedback to the user if correct answer was chosen. Still needs some minor tweaks

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,10 @@ let timerText = document.querySelector(".timerText");
 let questions = document.querySelector(".questions");
 let buttonContainer = document.querySelector(".buttonContainer");
 let scoreText = document.querySelector(".scoreText");
+let highScoreList = document.querySelector(".highScoreList");
+let feedbackText = document.querySelector(".feedbackText");
 
+//Timer for the Game
 let secondsLeft = 30;
 
 let questionNum = 0;
@@ -11,7 +14,11 @@ let questionNum = 0;
 let score = 0;
 
 let userAnswer;
+let userName;
+let userScore;
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+//console.log(highScores)
 
 let questionsArray = [
     {
@@ -51,9 +58,12 @@ let questionsArray = [
 */
 
 
+
+
 function startGame () {
+    
     //removes start button after being clicked.
-    startButton.setAttribute("style", "visibility: hidden");
+    startButton.setAttribute("style", "display:none");
 
     //timer
     let timeInterval = setInterval(function() {
@@ -81,17 +91,35 @@ function startGame () {
             }
             totalScore = score + secondsLeft
             */
-            
+
+            //End of game give score and ask for name input for High Score List
             userAlert = alert("Your score is " +score+ " out of "+questionsArray.length+ " questions");
-            
+            userName = prompt("Enter your name")
+
+            //Makes a finalScore Object in Local storage
+            const finalScore = {
+                score: score,
+                name: userName
+            }
+            //adds User name and Score to the Object
+            highScores.push(finalScore);
+            //Sorts the objects to Highest score to lowest
+            highScores.sort((a,b) => b.score - a.score);
+            //adds most recent user to local storage (but only top 5 are displayed on the page)
+            localStorage.setItem("highScores", JSON.stringify(highScores))
+            console.log(highScores)
             
         }
     }, 1000);
 
+
 };
 
 
+
+
 function questionContent() {
+
     //resets buttonContainer text to nothing for each time a choice is selected
     buttonContainer.textContent = ``;
 
@@ -100,7 +128,7 @@ function questionContent() {
         questions.textContent = "NO MORE QUESTIONS";
 
         console.log("QuestionNum = QuestionsArray");
-        secondsLeft = 0;
+        secondsLeft = 1;
 
     } else {
         // Writes the choices to the button Elements
@@ -123,15 +151,20 @@ function questionAnswer (e) {
 
     //get user input with button
     userAnswer = e.target.innerText;
-
+    
     //if user answers correctly adds +1 to score, otherwise 10sec are removed from secondsLeft
     if (userAnswer ===  questionsArray[questionNum].answer) {
         score += 1;
         scoreText.textContent = "Score: " +score;
+
+        feedbackText.textContent = "Previous Choice: CORRECT"
         console.log("the score is "+score);
     } else {
         secondsLeft -= 10;
+
+        feedbackText.textContent = "Previous Choice: INCORRECT"
         console.log("the timer was reduced");
+        console.log("the score is "+score);
     }
 
     //regardless if user answers correctly - moves on to the next question 
@@ -150,9 +183,24 @@ function questionAnswer (e) {
 }
 
 
+//Splices the highscore list in local storage to be only 5 (top 5 High Scores)
+highScores.splice(5);
+
 //Writes text to the UI
 timerText.textContent ='Timer: '+secondsLeft;
 scoreText.textContent = "Score: ";
+
+//writes High Score List on the screen
+//highScoreList.textContent = localStorage.getItem("highScores")
+
+document.getElementById("#1").innerHTML = highScores[0].name +" with a score of "+ highScores[0].score
+document.getElementById("#2").innerHTML = highScores[1].name +" with a score of "+ highScores[1].score
+document.getElementById("#3").innerHTML = highScores[2].name +" with a score of "+ highScores[2].score
+document.getElementById("#4").innerHTML = highScores[3].name +" with a score of "+ highScores[3].score
+document.getElementById("#5").innerHTML = highScores[4].name +" with a score of "+ highScores[4].score
+
+
+
 
 
 //listens for the StartButton to be clicked
